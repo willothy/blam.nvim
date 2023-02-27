6f52bdd264487b947d18687fa30ac292d6656f6e
Will Hopkins <willothyh@gmail.com>
2023-02-26 17:34:18 -0800

    v0.1.1

---
+++ README.md
@@ -0,0 +1,61 @@
+# Blam
+
+Inspired by VSCode and GitLens, this plugin simply adds blame info as virtual text to the end of your current line. Written in Rust using `git2` and `nvim-utils`.
+
+## Installation
+
+Install using your favorite package manager
+
+### Lazy.nvim
+```lua
+{
+    'willothy/blam.nvim',
+    build = 'make',
+    config = true
+}
+```
+
+### Packer.nvim
+```lua
+use('willothy/blam.nvim', {
+    run = 'make',
+    config = function()
+        require("blam").setup()
+    end
+})
+```
+
+## Usage
+
+Blam includes no mappings, just a few functions:
+```lua
+-- Toggles line blame (will update as you move the cursor)
+require("blam").toggle()
+
+-- Peeks the blame for the current line (will disappear when cursor is moved or after a timeout)
+require("blam").peek()
+```
+
+Here's my setup:
+```lua
+local blam = require("blam")
+vim.keymap.set("n", "<leader>b", blam.toggle()) 
+```
+
+## Configuration
+
+Blam comes with the following defaults:
+```lua
+{
+    -- Can be a hex color
+    -- or a table with fg and bg colors
+    -- or a highlight group
+    hl = "Comment",
+    -- Timeout before peek blame disappears
+    peek_timeout = 4000,
+    -- Whether line blame will be enabled on startup
+    enabled = true,
+}
+```
+
+
--- lua/blam.lua
+++ lua/blam.lua
@@ -1,38 +1,82 @@
 local M = {}
 
 local blame_ns = vim.api.nvim_create_namespace("blame")
+local blame_enabled = false
+local blame_cursor_move
 
-function M.add_virtual_text(text)
+local function add_virtual_text(text, opt)
     local bufnr = vim.api.nvim_get_current_buf()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 
     local opts = {
         end_line = line,
         id = 1,
-        virt_text = { { text, "Comment" } },
+        virt_text = { { text, opt.hl or "Comment" } },
         virt_text_pos = "eol",
-        --virt_text_win_col = 0
     }
     M.mark = vim.api.nvim_buf_set_extmark(bufnr, blame_ns, line - 1, col, opts)
 end
 
-function M.remove_virtual_text()
+local function remove_virtual_text()
     local bufnr = vim.api.nvim_get_current_buf()
     vim.api.nvim_buf_del_extmark(bufnr, blame_ns, M.mark)
 end
 
-M.core = require("blam.core")
-
-function M.peek_blame()
+local function show_line_blame()
+    M.remove_virtual_text()
     local line = vim.api.nvim_win_get_cursor(0)[1]
     local file = vim.fn.expand("%")
     local blame = M.core.get_line_blame(file, line)
     M.add_virtual_text(blame)
-    vim.defer_fn(M.remove_virtual_text, 5000)
+end
+
+M.config = {
+    enabled = true,
+    peek_timeout = 4000,
+    hl = "Comment",
+}
+
+function M.peek()
+    if blame_enabled then return end
+    show_line_blame()
+    vim.defer_fn(M.remove_virtual_text, M.config.peek_timeout)
+end
+
+function M.toggle()
+    if blame_enabled then
+        remove_virtual_text()
+        vim.api.nvim_del_autocmd(blame_cursor_move)
+        blame_cursor_move = nil
+    else
+        blame_cursor_move = vim.api.nvim_create_autocmd("CursorMove", {
+            callback = function()
+                show_line_blame()
+            end
+        })
+    end
+    blame_enabled = not blame_enabled
 end
 
 function M.setup(opt)
-    -- This isn't actually used yet, but some plugin managers call it automatically so I added it for compatibility
+    M.config = vim.tbl_deep_extend("force", M.config, opt)
+
+    if type(opt.hl) == 'string' then
+        if opt.hl:sub(1, 1) == '#' then
+            vim.highlight.create('BlamLine', { fg = opt.hl, bg = 'none' })
+            M.config.hl = 'BlamLine'
+        else
+            M.config.hl = opt.hl
+        end
+    elseif type(opt.hl) == "table" then
+        if opt.hl.fg and opt.hl.bg then
+            vim.highlight.create('BlamLine', { fg = opt.hl.fg, bg = opt.hl.bg })
+        end
+        M.config.hl = 'BlamLine'
+    end
+
+    blame_enabled = M.config.enabled
+
+    M.core = require('blam.core')
 end
 
 return M
